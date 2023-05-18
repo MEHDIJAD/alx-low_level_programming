61a86b0c4d2ad43ec214a211772f770f4e6699d0
MEHDIJAD <elmehdielgarouaz@gmail.com>
2023-05-18 14:19:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #!/bin/bash
  #include <stdio.h>
-
+/*
+ * main - Entry point
+ *
+ * Return: Awlays 0 (success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle\n");
