7497aeddb696ef858fb65e125aaf7cedebb5fc1d
MEHDIJAD <elmehdielgarouaz@gmail.com>
2023-05-20 09:07:44 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - main block
- * Return: 0
+ * main main - block
+ * Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful" "- Dora Korpar," 2015-10-19\n");
-return (0)
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
 }
