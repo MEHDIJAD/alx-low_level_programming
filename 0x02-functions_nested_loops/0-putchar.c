daa65b0fa63c69af10fbe641e934e009c36917fb
MEHDIJAD <elmehdielgarouaz@gmail.com>
2023-05-26 13:21:16 -0700

    task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-        char c[] = "_putchar";
+       	char c[] = "_putchar";
         int i = 0;
 
         while (i < 8)
         {
-                putchar(c[i]);
+		putchar(c[i]);
                 i++;
-        }
-        putchar('\n');
-
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
