8d061ace7234868c1b70382b0189483feb10d024
El Mehdi El Garouaz <eel-garo@e1r4p6.1337.ma>
2025-06-19 17:20:04 +0100

    add Task;3 + _putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 # define NAIN_H
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
+# include <unistd.h>
+
+void 	reset_to_98(int *n);
+void 	swap_int(int *a, int *b);
+int 	_strlen(char *s);
+void	 _puts(char *str);
+void  	_putchar(char c);
 
 #endif
