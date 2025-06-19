44fabe81b9ca46ce3f6f39701097da23d1304b3e
El Mehdi El Garouaz <eel-garo@e1r4p6.1337.ma>
2025-06-19 17:23:49 +0100

    add Task;4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 
 # include <unistd.h>
 
+void  	_putchar(char c);
 void 	reset_to_98(int *n);
 void 	swap_int(int *a, int *b);
 int 	_strlen(char *s);
 void	 _puts(char *str);
-void  	_putchar(char c);
+void 	print_rev(char *s);
 
 #endif
