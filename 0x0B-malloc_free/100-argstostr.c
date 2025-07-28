8e69af3eb67a1c1d8f62e2914e318cab9e53fc8e
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:44:32 +0100

    Add fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,47 @@
 #include "main.h"
 
-char *str_concat(char *s1, char *s2);
+
+/**
+ * str_concat - Concatenates two strings into a newly allocated memory space
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: Pointer to the newly allocated string containing s1 followed by s2,
+ *         or NULL if memory allocation fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int ci;
+	size_t biglen;
+	char *concat_str;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	biglen = _strlen(s1) + _strlen(s2);
+	concat_str = malloc(sizeof(char) * (biglen + 1));
+	if (!concat_str)
+		return (NULL);
+	ci = 0;
+	while (s1[i])
+		concat_str[ci++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		concat_str[ci++] = s2[i++];
+	concat_str[ci] = '\0';
+	return (concat_str);
+}
+
+/**
+ * argstostr - Concatenates all arguments into a single string
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: Pointer to new concatenated string with newlines
+ */
 
 char *argstostr(int ac, char **av)
 {
