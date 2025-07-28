f35b5e6f561af7b72647c346d1977fcf32e30236
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:41:34 +0100

    Add fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * free_grid - Frees a 2D grid previously created by alloc_grid
+ * @grid: Pointer to the 2D grid
+ * @height: Number of rows in the grid
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	while (i < height){
+	while (i < height)
+	{
 		free(grid[i]);
 		grid[i] = NULL;
 		i++;
