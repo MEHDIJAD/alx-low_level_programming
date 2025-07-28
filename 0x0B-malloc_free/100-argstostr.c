b05e249dce868e33e587720b334ebae7f3817f6e
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:01:12 +0100

    fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,31 @@
 #include "main.h"
 
+static char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int ci;
+	size_t biglen;
+	char *concat_str;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	biglen = _strlen(s1) + _strlen(s2);
+	concat_str = malloc(sizeof(char) * (biglen + 1));
+	if (!concat_str)
+		return (NULL);
+	ci = 0;
+	while (s1[i])
+		concat_str[ci++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		concat_str[ci++] = s2[i++];
+	concat_str[ci] = '\0';
+	return (concat_str);
+}
+
+
 char *argstostr(int ac, char **av)
 {
 	int i = 0;
