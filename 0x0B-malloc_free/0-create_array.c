d00cfbc646f3fb113c896fac734c5c361bb1ed6e
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:11:49 +0100

    add betty-doc for Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * create_array - Creates an array of chars and initializes it with a specific char
+ * @size: The size of the array to create
+ * @c: The character to initialize the array with
+ *
+ * Return: Pointer to the array, or NULL if size is 0 or malloc fails
+ */
+
 char *create_array(unsigned int size, char c)
 {
 	char			*array;
