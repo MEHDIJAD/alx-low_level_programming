bba02cd572d034fdd9e0bffa5ea2d6ba93f546a9
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:32:32 +0100

    Add betty-doc for Task 0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
+/**
+ * _bzero - Sets a line of integers to zero
+ * @line: Pointer to the line (array of integers)
+ * @n: Number of elements in the line
+ */
+
 void	_bzero(int *line, size_t n)
 {
 	size_t i;
 
 	i = 0;
 	while (i < n)
-		line[i++] = 0;
+	line[i++] = 0;
 }
+
+/**
+ * alloc_grid - Allocates a 2D grid of integers
+ * @width: Number of columns
+ * @height: Number of rows
+ *
+ * Return: Pointer to the 2D grid, or NULL if memory allocation fails
+ */
 
 int **alloc_grid(int width, int height)
 {
@@ -23,8 +37,10 @@ int **alloc_grid(int width, int height)
 	while (y < height)
 	{
 		grid[y] = malloc(sizeof(int) * width);
-		if (!grid[y]){
-			while (--y){
+		if (!grid[y])
+		{
+			while (--y)
+			{
 				free(grid[y]);
 			}
 			free(grid);
