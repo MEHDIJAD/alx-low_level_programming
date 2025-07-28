823fd68c0ba16de0aa3db6639a516a5e53fc811a
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:46:51 +0100

    Add fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+/**
+ * _strlen - Return the length of the string passed to it
+ * @str: The string to evaluate
+ *
+ * Return: The length of the string
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
 
 /**
  * str_concat - Concatenates two strings into a newly allocated memory space
