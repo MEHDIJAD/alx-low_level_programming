e0c38dcd8e9aee6c7baa1bf8a34d3d2aee6edefe
eel-garo <126072316+MEHDIJAD@users.noreply.github.com>
2025-07-28 17:23:37 +0100

    Add fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,23 @@
 #include <string.h>
 
 /**
+ * _strlen - Return the length of the string passed to it
+ * @str: The string to evaluate
+ *
+ * Return: The length of the string
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+/**
  * _strdup - Returns a pointer to a newly allocated space in memory,
  *           which contains a copy of the string given as a parameter
  * @str: The string to duplicate
@@ -10,19 +27,11 @@
  *         or if memory allocation fails
  */
 
-size_t _strlen(const char *str)
-{
-	size_t len = 0;
-
-	while (str[len]){
-		len++;
-	}
-	return (len);
-}
-
 
 char *_strdup(char *str)
 {
+	if (!str)
+		return (NULL);
 	size_t len = _strlen(str);
 	size_t i = 0;
 	char *cpy = malloc(sizeof(char) * (len + 1));
